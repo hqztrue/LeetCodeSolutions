f837f70e5500cd92b205530d6b72ce050b2a2b1f
hqztrue <hqztrue@sina.com>
2023-07-08 23:57:46 -0500

    .

---
+++ 0501-0600/572. Subtree of Another Tree 421ms.py
@@ -0,0 +1,9 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        return str(subRoot) in str(root)
